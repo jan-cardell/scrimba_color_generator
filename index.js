4908fcd522a53eb210f4e8a70fc1a79cd93c5dd3
jan-cardell <janpetercardell@outlook.de>
2026-08-09 22:47:38 +0200

    Additional api and first css styling

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 const inputForm = document.getElementById('input-form')
+const colorOut = document.getElementById('color-out')
 
 inputForm.addEventListener('submit', function(e){
     e.preventDefault()
     const inputColor = document.getElementById('input-color').value
     const inputScheme = document.getElementById('input-scheme').value
 
-    fetch ('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=6')
+    fetch (`https://www.thecolorapi.com/scheme?hex=0047AB&mode=${inputScheme}&count=4`)
         .then(res => res.json())
-        .then(data => console.log(data.colors[1].hex.value))
+        .then(function(data){ data.colors.forEach(function(color){
+            colorOut.innerHTML += `<div>`
+            console.log(color.hex.value)
+        })
+        })
 })
