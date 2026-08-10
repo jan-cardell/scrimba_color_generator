4f0981c7621d859cfef2e662bacb0662b5d42383
jan-cardell <janpetercardell@outlook.de>
2026-08-10 18:43:35 +0200

    Adding color generation feature

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 const inputForm = document.getElementById('input-form')
-const colorOut = document.getElementById('color-out')
 
 inputForm.addEventListener('submit', function(e){
     e.preventDefault()
     const inputColor = document.getElementById('input-color').value
     const inputScheme = document.getElementById('input-scheme').value
+    
+    document.getElementById('color-set').style.backgroundColor= inputColor
 
-    fetch (`https://www.thecolorapi.com/scheme?hex=0047AB&mode=${inputScheme}&count=4`)
+    fetch (`https://www.thecolorapi.com/scheme?hex=${inputColor.slice(1)}&mode=${inputScheme}&count=4`)
         .then(res => res.json())
-        .then(function(data){ data.colors.forEach(function(color){
-            colorOut.innerHTML += `<div>`
-            console.log(color.hex.value)
+        .then(function(data){ data.colors.forEach(function(color, index){
+            document.getElementById(`color${index}`).style.backgroundColor= color.hex.value
         })
         })
 })
